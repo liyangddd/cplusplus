0bfbf2eac78ca51f87fadd0d89f474e90c6a2140
liyangddd <977709619@qq.com>
2015-07-28 11:15:49 +0800

    Update Call_constructor.cpp

--- Call_constructor.cpp
+++ Call_constructor.cpp
@@ -1,4 +1,5 @@
 /*
+第一个例子：
 复制控制（复制构造函数、赋值操作符、析构函数）的调用时机测试(一)
 */
 class A {
@@ -72,5 +73,76 @@ int main() {
 	//使用了五次默认构造函数和复制构造函数，先使用A的默认构造函数创建临时值
 	//来初始化avec，然后使用复制构造函数将临时值复制到avec的每个元素。
 	vector<A> avec(5);  
+	return 0;
+}
+
+
+/*
+第二个例子：
+复制控制（复制构造函数、赋值操作符、析构函数）的调用时机测试(二)
+*/
+class Exmpl {
+public:
+	Exmpl() {	cout << "Exmpl()" << endl;  }
+	Exmpl(const Exmpl&) {  cout << "拷贝构造函数" << endl;  }
+	Exmpl& operator= (const Exmpl & rhs) {
+		cout << "oprator=" << endl;
+		return *this;
+	}
+	~Exmpl() {  cout << "~Exmpl" << endl; }
+};
+
+void func1(Exmpl obj) {}   //形参为Exmpl对象
+void func2(Exmpl& obj) {}  //形参为Exmpl对象的引用
+Exmpl func3() {
+	Exmpl obj;    return obj;
+}
+
+const Exmpl& func4(const Exmpl& obj) {
+	return obj;
+}
+int main() {
+	//调用默认构造函数创建Exmpl对象obj
+	Exmpl obj;
+
+	//调用复制构造函数
+	//将形参Exmpl对象创建为实参Exmpl对象的副本
+	//函数执行完毕后，调用析构函数撤销形参Exmpl对象
+	func1(obj); 
+
+	//形参为对象的引用，因此不需要调用复制构造函数传递实参
+	func2(obj);
+
+	//调用默认构造函数创建局部Exmpl对象
+	//函数返回时调用复制构造函数创建作为返回值副本的Exmpl对象
+	//然后调用析构函数撤销局部对象
+	//调用赋值操作符完成赋值操作
+	//调用析构函数撤销作为返回值副本的Exmpl对象
+	obj = func3();
+
+	//函数形参和返回值都是引用，不会调用(拷贝)构造函数
+	func4(obj);
+
+	//func4不会调用任何复制控制操作
+	//由于exm也是一个引用，因此下一句不会调用任何构造、析构函数
+	const Exmpl& exm = func4(obj);
+
+	//调用拷贝构造函数来将exm2对象初始化为func4返回的对象的引用
+	const Exmpl exm2 = func4(obj);
+
+	//调用默认构造函数动态创建对象
+	//该对象需要手动才能销毁
+	Exmpl *p = new Exmpl;
+
+	//三次调用默认构造函数创建临时对象
+	//三次调用复制构造函数将临时对象复制到容器evec的每个元素
+	//调用析构函数撤销临时对象
+	vector<Exmpl> evec(3);
+	
+	//调用析构函数撤销动态创建的对象
+	delete p;
+
+	//evec和obj的生命结束，调用析构函数撤销
+	//evec撤销需要调用三次析构函数
 	return 0;
 }
